a3292911949f58507fea028dce8f60d861c073e1
Claudio Cordova <claudio_cordova@yahoo.com>
2023-03-31 17:35:56 -0700

    Added EC2

--- create-stack.sh
+++ create-stack.sh
@@ -1,8 +1,22 @@
 
+
+if [ -z "$1" ]; then
+      MODE=FARGATE
+elif [ "$1" == "EC2" ]; then
+      MODE=$1
+elif [ "$1" == "FARGATE" ]; then
+      MODE=$1
+else
+    echo "Wrong parameter 1 MODE: "$1
+    exit 1 
+fi
+
+
+
 cd ../poker-hand-analyzer-microservice-springboot-aws-ecs
-./create-stack.sh
+./create-stack.sh $MODE
 cd ../poker-hand-analyzer-microservice-springboot-aws-ecs-iac
-./create-stack.sh
+./create-stack.sh $MODE
 cd ../poker-hand-analyzer-microservice-springboot-aws-ecs-cicd
 ./create-stack.sh
 cd ../poker-hand-analyzer-microservice-springboot-aws-ecs-start
