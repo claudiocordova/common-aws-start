f3888ef1f415ee3641364b57195dd97e335601ee
Claudio Cordova <claudio_cordova@yahoo.com>
2023-04-25 10:16:33 -0700

    First

--- create-stack.sh
+++ create-stack.sh
@@ -6,58 +6,62 @@ if [ -z "$1" ]; then
 elif [ "$1" == "EKS_EC2" ]; then
       MODE=$1
 
-    cd ../poker-hand-analyzer-microservice-springboot-aws-ecs
-    ./create-stack.sh $MODE
+    cd ../poker-hand-analyzer-microservice-springboot
+    ./eks-create-stack.sh $MODE
     cd ../common-aws-vpc
     ./create-stack.sh $MODE
     cd ../common-aws-eks
     ./create-stack.sh $MODE
+    cd ../common-aws-cicd
+    ./eks-create-stack.sh $MODE        
     cd ../common-aws-start
 
 elif [ "$1" == "EKS_FARGATE" ]; then
       MODE=$1
 
-    cd ../poker-hand-analyzer-microservice-springboot-aws-ecs
-    ./create-stack.sh $MODE
+    cd ../poker-hand-analyzer-microservice-springboot
+    ./eks-create-stack.sh $MODE
     cd ../common-aws-vpc
     ./create-stack.sh $MODE
     cd ../common-aws-eks
     ./create-stack.sh $MODE
+    cd ../common-aws-cicd
+    ./eks-create-stack.sh $MODE    
     cd ../common-aws-start
 
 
 elif [ "$1" == "ECS_EC2" ]; then
       MODE=$1
 
-    cd ../poker-hand-analyzer-microservice-springboot-aws-ecs
-    ./create-stack.sh $MODE
+    cd ../poker-hand-analyzer-microservice-springboot
+    ./ecs-create-stack.sh $MODE
     cd ../common-aws-vpc
     ./create-stack.sh $MODE
     cd ../common-aws-ecs
     ./create-stack.sh $MODE
     cd ../common-aws-cicd
-    ./create-stack.sh $MODE
+    ./ecs-create-stack.sh $MODE
     cd ../common-aws-start
 
 
 elif [ "$1" == "ECS_FARGATE" ]; then
       MODE=$1 
 
-    cd ../poker-hand-analyzer-microservice-springboot-aws-ecs
-    ./create-stack.sh $MODE
+    cd ../poker-hand-analyzer-microservice-springboot
+    ./ecs-create-stack.sh $MODE
     cd ../common-aws-vpc
     ./create-stack.sh $MODE
     cd ../common-aws-ecs
     ./create-stack.sh $MODE
     cd ../common-aws-cicd
-    ./create-stack.sh $MODE
+    ./ecs-create-stack.sh $MODE
     cd ../common-aws-start
 
 elif [ "$1" == "EC2" ]; then
       MODE=$1
 
-    cd ../poker-hand-analyzer-microservice-springboot-aws-ecs
-    ./create-stack.sh $MODE
+    cd ../poker-hand-analyzer-microservice-springboot
+    ./ec2-create-stack.sh $MODE
     cd ../common-aws-vpc
     ./create-stack.sh $MODE
     cd ../common-aws-ec2
@@ -68,8 +72,8 @@ elif [ "$1" == "EC2" ]; then
 elif [ "$1" == "EC2_DOCKER" ]; then
       MODE=$1
 
-    cd ../poker-hand-analyzer-microservice-springboot-aws-ecs
-    ./create-stack.sh $MODE
+    cd ../poker-hand-analyzer-microservice-springboot
+    ./ec2-create-stack.sh $MODE
     cd ../common-aws-vpc
     ./create-stack.sh $MODE
     cd ../common-aws-ec2
